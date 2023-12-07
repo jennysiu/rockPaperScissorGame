84efea807059bc624b8dca675921853f356a3137
jennysiu <jenny.siu79@gmail.com>
2023-12-07 22:12:21 +0000

    error handling lines added to deal with invalid responses

--- script.js
+++ script.js
@@ -25,11 +25,14 @@ let draw = 0;
 
 // for loop to go 10 rounds
 for (rounds = 0; rounds < 10; rounds++){
-  // ***USER TURN
-  let userTurn = prompt("Rock, paper, or scissors? (Use: R, P, or S)"); 
-
   // choices
   let choices = ["R", "P", "S"];
+  
+  // ***USER TURN
+  let userTurn = prompt("Rock, paper, or scissors? (Please type: R, P, or S)").toUpperCase(); 
+    while (!choices.includes(userTurn)) {
+      userTurn = prompt("INVALID choice. Please type R, P, or S!")
+    } 
   
   // ***BOT TURN
   // random number generator
@@ -37,52 +40,52 @@ for (rounds = 0; rounds < 10; rounds++){
   // map the random number to choices
   let randomChoice = choices[randomIndex];
   let botTurn = randomChoice;
-  alert(`Bot turn: ${botTurn}`)
+  // alert(`Bot turn: ${botTurn}`)
 
   let roundMessage = `Your turn: ${userTurn} \nBot turn: ${botTurn}`;
-  alert(roundMessage);
+  // alert(roundMessage);
 
   // ***CONDITIONALS
   if (userTurn == "R" && botTurn =="S") {
     win = win +1; 
-    alert("You win!")
+    alert(roundMessage + "\nYou win!")
   } else if (userTurn == "R" && botTurn =="R") {
     draw = draw + 1;
-    alert("It's a draw!")
+    alert(roundMessage + "\nIt's a draw!")
   } else if (userTurn == "R" && botTurn =="P") {
     lose = lose + 1;
-    alert("Sorry, you lose.")
+    alert(roundMessage + "\nSorry, you lose.")
     // papers
   } else if (userTurn == "P" && botTurn =="R") {
     win = win +1;
-    alert("You win!")
+    alert(roundMessage + "\nYou win!")
   } else if (userTurn == "P" && botTurn =="P") {
     draw = draw + 1;
-    alert("It's a draw!")
+    alert(roundMessage + "\nIt's a draw!")
   } else if (userTurn == "P" && botTurn =="S") {
     lose = lose + 1;
-    alert("Sorry, you lose.")
+    alert(roundMessage + "\nSorry, you lose.")
     // scissors
   } else if (userTurn == "S" && botTurn =="P") {
     win = win +1;
-    alert("You win!")
+    alert(roundMessage + "\nYou win!")
   } else if (userTurn == "S" && botTurn =="S") {
     draw = draw + 1;
-    alert("It's a draw!")
+    alert(roundMessage + "\nIt's a draw!")
   } else if (userTurn == "S" && botTurn =="R") {
     lose = lose + 1;
-    alert("Sorry, you lose.")
+    alert(roundMessage + "\nSorry, you lose.")
   } 
     
     // Ask if user wants to play again
-  let playAgain = prompt("Play again? (Y or N)");
-    if (playAgain == "Y") {
-    } else if (playAgain == "N") {
+  let playAgain = confirm("Play again?");
+    if (playAgain == true) {
+    } else {
       break;
     } 
 }
 
 // Alert scores
-let endMessage = `Wins: ${win} \nLoses: ${lose} \nDraws: ${draw}` 
+let endMessage = `Wins: ${win} \nLoses: ${lose} \nDraws: ${draw} \nThanks for playing!!` 
 alert(endMessage);
 
